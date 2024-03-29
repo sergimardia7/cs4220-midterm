1acfd5512e2ba23046e41e6085c898a2cceb052e
sergimardia7 <Sergimardia@gmail.com>
2024-03-28 22:18:39 -0700

    New Implementation added

--- api.js
+++ api.js
@@ -1,33 +1,100 @@
-// The File to interact with the Basketball API
 import axios from 'axios';
 
-//Base for the API Call
-const base = 'http://site.api.espn.com/apis/site/v2/sports/basketball';
+const base = 'http://site.api.espn.com/apis/site/v2/sports/basketball/nba';
 
-//Pulls information based on user request
-export const buildScores = async () => {
-    try {
-        // Need to find a way to choose from NBA, WNBA, MNCAA, WNCAA
-        //choice === true;
-        const scores = `${base}/nba/scoreboard`;
-        const response = await axios.get(scores);
+export const buildScoreboard = async(date) => {
+    try{
+        
+        const statsURL = `${base}/scoreboard?dates=${date}`;
+
+        const response = await axios.get(statsURL);
+
         return response.data;
-
-        //http://site.api.espn.com/apis/site/v2/sports/basketball/NBA/scoreboard
-    } catch (error) {
+    }catch(error){
         return error;
     }
 };
 
-// Searches thru NBA Stats
-export const teamSearch = async (teamName) => {
-    try {
-        const url = `${base}/nba/teams/${teamName}`;
+export const teamStats = async(team) => {
+    try{
+        const teamStatics = `${base}/teams/${team}`;
 
-        const response = await axios.get(url);
+        const response = await axios.get(teamStatics);
 
         return response.data;
-    } catch (error) {
+    }catch(error){
         return error;
     }
 };
+
+// export async function getScores() {
+//     try {
+//         const scoreboardData = await buildScoreboard();
+//         //'competitors' properties
+//         const events = scoreboardData.events;
+//         events.forEach(event => {
+//             const competitions = event.competitions;
+//             competitions.forEach(competition => {
+//                 const competitors = competition.competitors;
+//                 competitors.forEach(competitor => {
+//                     const team = competitor.team.displayName;
+//                     const score = competitor.score;
+//                     console.log(`${team} : ${score}`);
+//                 });
+//             });
+//         });
+//     } catch (error) {
+//         console.error('Error fetching scores:', error);
+//     }
+// }
+
+
+// export const buildStats = async(date, team) => {
+//     try{
+        
+//         const statsURL = `${base}/teams/${team}?nextEvent=401585683`;
+
+//         let response = await axios.get(statsURL);
+
+//         return response.data;
+//     }catch(error){
+//         return error;
+//     }
+// };
+
+//Yusef Code
+export const printScores = async (dates, shorthand) => {
+    const { data } = await axios({
+        method: 'get',
+        url: `${base}/scoreboard`,
+        params: {
+            dates
+        },
+    });
+
+    for (const event in data.events) {
+        if (data.events[event].shortName.includes(shorthand)) {
+            console.log(data.events[event].competitions[0]);
+        }
+    }
+};
+
+export const printTeams = async () => {
+    const { data } = await axios({
+        method: 'get',
+        url: 'http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams',
+    });
+    const teams = data.sports[0].leagues[0].teams;
+    const abbreviations = {};
+
+    for (const t in teams) {
+        abbreviations[teams[t].team.displayName.toLowerCase()] =
+            teams[t].team.abbreviation;
+    }
+
+    console.log(abbreviations);
+};
+
+
+
+
--- app.js
+++ app.js
@@ -1,70 +1,46 @@
 import * as api from './api.js';
-import { checkbox } from '@inquirer/checkbox';
-import * as db from './db.js';
 
-export const handleSearch = async (teamName, useCache) => {
+const test = await api.printScores('20240123','lal');
+//const test2 = await api.printTeams();
+//const test3 = await api.getScores();
+
+// Danny's Score Gathering Code
+async function getScores(dates) {
     try {
-        const searchResult = await searchNBATeam(teamName);
-        await create('history', {search: teamName, result: searchResult.length});
-        const selectedItem = searchResult.find(team => team.displayName.toLowerCase() === teamName.toLowerCase());
-        if (!selectedItem) {
-            throw new Error(`Team '${teamName}' not found.`);
-        }
-        let data;
-        if (useCache) {
-            const cachedData = await find('search_cache', selectedItem.id);
-            if (cachedData){
-                data = cachedData;
-            } else{
-                data = await buildScores();
-                await create('search_cache', data);
-            }
-        } else {
-            data = await buildScores();
-            await create('search_cache', data);
-        }
-        console.log('Information on selected team: ');
-        console.log(data);
+        const scoreboardData = await api.buildScoreboard(dates);
+        //'competitors' properties
+        const events = scoreboardData.events;
+        events.forEach(event => {
+            const competitions = event.competitions;
+            competitions.forEach(competition => {
+                const competitors = competition.competitors;
+                competitors.forEach(competitor => {
+                    const team = competitor.team.displayName;
+                    const score = competitor.score;
+                    console.log(`${team} : ${score}`);
+                });
+            });
+        });
     } catch (error) {
-        console.error(error.message);
+        console.error('Error fetching scores:', error);
     }
-};
+}
 
-const _printConsole = () => {
 
-};
-
-export const buildStats = async (args) => {
+const getTeamStats = async(team) => {
     try{
-        const stats = await api.teamSearch(args);
 
-        console.log(stats);
-        return stats;
+        const teamStats = await api.teamStats(team);
         
+        const teamRecord = teamStats.sports[0].leagues[0].teams;
+
+        console.log(teamRecord);
+
+
     }catch(error){
         return error;
     }
 };
 
-export const buildScore = async (args) => {
-    try{
-
-        const scores = await api.buildScores();
-        console.log(scores);
-        return scores; 
-        
-    }catch(error){
-        return error;
-    }
-};
-
-export const handleHistory = async () => {
-    try {
-        const history = await find('history');
-        console.log('Search History:');
-        console.log(history);
-    } catch (error) {
-        console.error(error.message);
-        return error; 
-    }
-};
+//console.log(getScores('20240125'));
+console.log(getTeamStats('lal'));
