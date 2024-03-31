9885e22f54de7f2ea53b7143252aac15617c5f84
jessewrx <126747123+jessewrx@users.noreply.github.com>
2024-03-31 00:45:24 -0700

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,2 +1,56 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import { getScores } from './app.js';
+import { getSearchHistory } from './api.js';
+
+import input from '@inquirer/input';
+
+
+
+
+
+
+// Define command-line options
+yargs(hideBin(process.argv))
+    .command({
+        command: 'process-date',
+        describe: 'Process a date in format yyyyMMdd',
+        builder: {
+            date: {
+                alias: 'd',
+                describe: 'Date in format yyyyMMdd',
+                type: 'string',
+                demandOption: true // Make the option required
+            }
+        },
+        handler: (argv) => {
+            // Extract the date from command-line arguments
+            
+            const { date } = argv;
+
+            // Pass the date to the app module
+            getScores(date);
+        }
+    })
+   
+    .command(
+        'history', 
+        'View search history',
+        {},
+        async () => {
+            try {
+                const searchHistory = await getSearchHistory();
+                if (searchHistory && searchHistory.length > 0) {
+                    console.log('Search History:');
+                    searchHistory.forEach((item, index) => {
+                        console.log(`${index + 1}. Keyword: ${item.keyword}, Date: ${item.date}`);
+                    });
+                } else {
+                    console.log('No search history available.');
+                }
+            } catch (error) {
+                console.error('Error getting search history:', error.message);
+            }
+        })
+    .help()
+    .argv; 
