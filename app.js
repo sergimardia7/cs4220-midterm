13acd93a058f618e5489b9dd1030715b7c29d4b2
sergimardia7 <Sergimardia@gmail.com>
2024-05-09 18:34:16 -0700

    Now Working!

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
 // the file for our application logic (aka logic to play 5 card poker)
 import { select } from '@inquirer/prompts';
 import * as api from './api.js';
-//import * as db from './db.js';
+import * as db from './db.js';
+
+const _printConsole = async (brewsId) => {
+    const brewery = await api.getByIdentifier(brewsId);
+    
+    console.log(`Type: ${brewery.brewery_type}`);
+    console.log(`Address: ${brewery.address_1}, ${brewery.state_province}, ${brewery.country} ${brewery.postal_code}`);
+    console.log(`Website: ${brewery.website_url}`);
+    console.log(`Phone: ${brewery.phone}`);
+
+    //console.log(brewery);
+};
+
+const _brewList = async (brews) => {
+    const displayBrews = brews.map((brew) => {
+        return {name: `${brew.name}`, value: brew.id};
+    });
+
+    return await select({
+        message: 'Select Brewery of Interest for more Information',
+        choices: displayBrews,
+    });
+};
+
+
 
 
 export const brewSearch = async (args) => {
     try{
         const breweries = await api.searchByKeyword(args.brew);
+        const userChoice = await _brewList(breweries);
 
-        console.log(breweries);
+        _printConsole(userChoice);
+
+        //console.log(userChoice);
+
+        const entry = {
+            id: userChoice
+        };
+
+        await db.create('search_history', userChoice);
 
     }catch(err){
         console.error(err);
@@ -29,7 +62,12 @@ export const brewIDSearch = async (args) => {
 
 export const history = async () => {
     try{
-        
+        const historySearch = await db.find('search_history');
+        const previousSearch = historySearch.pop();
+        const brewery = await api.getByIdentifier(previousSearch);
+
+        console.log(brewery);
+
     }catch(err){
         console.log(err);
     }
